07add84b97e939a3760e159d5ac4314821437cfe
Jay Vince Serato <jayvince.serato@gmail.com>
2024-08-21 17:11:38 +0800

    Add array list

---
+++ Aug20/arraylist.h
@@ -0,0 +1,39 @@
+#include "list.h"
+#include <iostream>
+using namespace std;
+// ArrayList is a subclass of List
+class ArrayList : public List {
+	int array[5];
+	int size = 0;
+	
+	public:
+	void add(int num) {
+		cout << "Add " << num << endl;
+		array[size++] = num;
+	}
+	
+	int get(int pos) {
+		return array[pos-1];
+	}
+	
+	int remove(int num) {
+		for (int i = 0; i < size; i++) {
+			if (num == array[i]) {
+				for (int j = i; j < size-1; j++) {
+					array[j] = array[j+1];
+				} 
+				size--;
+				return i+1;
+			}
+		}
+		return -1;
+	}
+	
+	void print() {
+		cout << "Size: "<< size <<endl;
+		for (int i = 0; i < size; i++) {
+			cout << array[i] << " ";
+		}
+		cout << endl;
+	}
+};
